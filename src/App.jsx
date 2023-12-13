fb3acd77ca3c54f74f65bcfa5122e093802adcf2
AKD <contact.mousom@gmail.com>
2023-12-13 17:00:08 +0600

    Coursor animated feature done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { Outlet } from "react-router-dom"
 import MainLayout from "./Layouts/MainLayout/MainLayout"
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
 
 
   return (
     <>
+    <AnimatedCursor
+      innerSize={8}
+      outerSize={8}
+      color='193, 11, 111'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link',
+        {
+          target: '.custom',
+          options: {
+            innerSize: 12,
+            outerSize: 12,
+            color: '255, 255, 255',
+            outerAlpha: 0.3,
+            innerScale: 0.7,
+            outerScale: 5
+          }
+        }
+      ]}
+    />
       <MainLayout>
         <Outlet></Outlet>
         
